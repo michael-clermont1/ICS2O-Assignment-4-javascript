1f3aaae037d836848450a58c5ed87edcdc957380
Ubuntu <ubuntu@ip-172-31-93-92.ec2.internal>
2020-06-19 14:22:13 +0000

    Added index.html and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var lol=prompt("Please enter circle")
+if(lol==="circle"){
+    var tr1=Math.PI
+    var tr2=Number(prompt("Enter the diameter."))
+    var tr3=0.25*Math.PI
+    confirm((tr2*tr2*tr3))
+    prompt("Now for the circumference of a circle.(Just click next)")
+    prompt((tr1*tr2))
+}
